689b299ff1b928e8e711c6fa90c537993f0dcae1
perfectwan <wanzekim@gmail.com>
2021-08-31 00:47:18 +0900

    ---

---
+++ im-sprint-beesbeesbees/src/Grub.js
@@ -0,0 +1,13 @@
+class Grub {
+  constructor(age,color,food){
+    this.food = 'jelly';
+    this.age = 0;
+    this.color = 'pink';
+  }
+
+  eat(){
+    return 'Mmmmmmmmm jelly';
+  };
+}
+
+module.exports = Grub;
